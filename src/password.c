271aec7f5715f7af2e9e1f52c5382621612cd895
salmon <elis.staaf@proton.me>
2026-02-24 16:34:35 +0100

    fix VERY FATAL AND DANGEROUS BUG
    previously, if you entered an empty master password, it would bypass the
    login menu.

--- src/password.c
+++ src/password.c
@@ -81,6 +81,10 @@ int verify_master_password(char* password, unsigned char* hash) {
     unsigned char password_hash[MAX_HASH_LEN];
     hash_password(password, password_hash);
 
+    if (!strcmp(password, "")) {
+        return 0;
+    }
+
     for (int i = 0; password[i] != '\0'; i++) {
         if (password_hash[i] != hash[i])
             return 0;
